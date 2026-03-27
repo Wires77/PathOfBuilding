073ad087c5c72a28f24a03d2e2ce612712969f66
Wires77 <Wires77@users.noreply.github.com>
2026-03-26 23:01:39 -0500

    Remove debugging lines

--- src/Export/Launch.lua
+++ src/Export/Launch.lua
@@ -13,9 +13,6 @@ launch = { }
 SetMainObject(launch)
 
 function launch:OnInit()
-	package.cpath = package.cpath .. ";c:/Users/trevo/.vscode/extensions/tangzx.emmylua-0.9.22-win32-x64/debugger/emmy/windows/x64/?.dll"
-	local dbg = require("emmy_core")
-	dbg.tcpListen("localhost", 9966)
 	self.devMode = true
 	self.subScripts = { }
 	RenderInit()
